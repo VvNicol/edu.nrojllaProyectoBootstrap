1615523bef72f80860911487b216edae5ce165d3
VvNicol <150023181+VvNicol@users.noreply.github.com>
2024-05-16 07:36:11 +0200

    Add files via upload

--- webapp/js/js.js
+++ webapp/js/js.js
@@ -1,11 +1,56 @@
 //PARA VERIFICAR SI EXISTEN LOS ID
 document.addEventListener("DOMContentLoaded", function() {
-	//******************************PAGINA TIENDA***************************//
+
+	//******************************PAGINA COMPRA**************************//
+
+	//*BOTON FORMULARIO */
+	// Verificar si el elemento "siguiente" existe antes de agregar el event listener
+	var siguienteBtn = document.getElementById("siguiente");
+	if (siguienteBtn) {
+		siguienteBtn.addEventListener("click", function() {
+			var nombre = document.getElementById("inputNombre").value.trim();
+			var apellidos = document.getElementById("inputApellidos").value.trim();
+			var direccion = document.getElementById("inputAddress").value.trim();
+			var ciudad = document.getElementById("inputCity").value.trim();
+			var cp = document.getElementById("inputZip").value.trim();
+			var provincia = document.getElementById("inputState").value.trim();
+			var aceptarTerminos = document.getElementById("aceptarTerminos").checked;
+
+			if (nombre === "" || apellidos === "" || direccion === "" || ciudad === "" || cp === "" || provincia === "" || !aceptarTerminos) {
+				alert("Por favor complete todos los campos requeridos (Nombre, Apellidos, Dirección, Ciudad, CP y Provincia) y acepte los términos y condiciones para proceder al pago.");
+				return; // Detener la ejecución del código si falta algún campo o no se han aceptado los términos y condiciones
+			}
+
+			// Si todos los campos están completos y se han aceptado los términos y condiciones, continuar con la redirección
+			window.location.href = "pago.html";
+		});
+	}
+
+
+	/*BOTON BORRAR FORMULARIO*/
+
+	var borrarBtn = document.getElementById("borrar");
+	if (borrarBtn) {
+		borrarBtn.addEventListener("click", function() {
+			document.getElementById("inputNombre").value = "";
+			document.getElementById("inputApellidos").value = "";
+			document.getElementById("inputEmail4").value = "";
+			document.getElementById("inputAddress").value = "";
+			document.getElementById("inputAddress2").value = "";
+			document.getElementById("inputState").value = "";
+			document.getElementById("inputZip").value = "";
+			document.getElementById("inputCity").value = "";
+		});
+	}
+	//***********************PAGINA PAGO.HTML******************************//
+
+	/*Agregar al carrito las compras de la tienda*/
+
 	iniciarCarrito();
 
 
 	function iniciarCarrito() {
-		const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+		let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 		let total = carrito.reduce((acc, item) => acc + item.valor, 0);
 
 		mostrarCarrito();
@@ -20,6 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				total += valor;
 				localStorage.setItem('carrito', JSON.stringify(carrito));
 				mostrarCarrito();
+				alert("Se ha agregado al carrito");  // Mostrar alerta
 			});
 		});
 
@@ -32,14 +78,17 @@ document.addEventListener("DOMContentLoaded", function() {
 				if (carrito.length > 0) {
 					carrito.forEach((item, index) => {
 						carritoHTML += `
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">${item.titulo}</h6>
-                        </div>
-                        <span class="text-muted">${item.valor} €</span>
-                        <button class="btn btn-sm btn-danger eliminar-item" data-indice="${index}">Eliminar</button>
-                    </li>
-                `;
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">${item.titulo}</h6>
+                                </div>
+                                <span class="text-muted">${item.valor} €</span>
+                                <div>
+                                    <button class="btn btn-sm btn-danger eliminar-item" data-indice="${index}">Eliminar</button>
+                                    <button class="btn btn-sm btn-success agregar-item" data-indice="${index}">Agregar</button>
+                                </div>
+                            </li>
+                        `;
 					});
 				} else {
 					carritoHTML += "<p>No hay artículos en el carrito.</p>";
@@ -53,6 +102,8 @@ document.addEventListener("DOMContentLoaded", function() {
 				botonesEliminar.forEach(boton => {
 					boton.addEventListener('click', () => {
 						const indice = boton.dataset.indice;
+						// Restar el valor del artículo al total
+						total -= carrito[indice].valor;
 						// Eliminar el elemento correspondiente del carrito
 						carrito.splice(indice, 1);
 						// Actualizar el contenido del carrito en el almacenamiento local
@@ -61,54 +112,24 @@ document.addEventListener("DOMContentLoaded", function() {
 						mostrarCarrito();
 					});
 				});
+
+				// Escuchar eventos de clic en los botones de agregar
+				const botonesAgregar = document.querySelectorAll('.agregar-item');
+				botonesAgregar.forEach(boton => {
+					boton.addEventListener('click', () => {
+						const indice = boton.dataset.indice;
+						// Incrementar el total con el valor del artículo
+						total += carrito[indice].valor;
+						// Agregar el mismo artículo de nuevo al carrito
+						carrito.push(carrito[indice]);
+						// Actualizar el contenido del carrito en el almacenamiento local
+						localStorage.setItem('carrito', JSON.stringify(carrito));
+						// Volver a mostrar el carrito
+						mostrarCarrito();
+					});
+				});
 			}
 		}
-
-
-		//******************************PAGINA COMPRA**************************//
-
-		//*BOTON FORMULARIO */
-
-		// Verificar si el elemento "siguiente" existe antes de agregar el event listener
-		var siguienteBtn = document.getElementById("siguiente");
-		if (siguienteBtn) {
-			siguienteBtn.addEventListener("click", function() {
-				var nombre = document.getElementById("inputNombre").value.trim();
-				var apellidos = document.getElementById("inputApellidos").value.trim();
-				var direccion = document.getElementById("inputAddress").value.trim();
-				var ciudad = document.getElementById("inputCity").value.trim();
-				var cp = document.getElementById("inputZip").value.trim();
-				var provincia = document.getElementById("inputState").value.trim();
-				var aceptarTerminos = document.getElementById("aceptarTerminos").checked;
-
-				if (nombre === "" || apellidos === "" || direccion === "" || ciudad === "" || cp === "" || provincia === "" || !aceptarTerminos) {
-					alert("Por favor complete todos los campos requeridos (Nombre, Apellidos, Dirección, Ciudad, CP y Provincia) y acepte los términos y condiciones para proceder al pago.");
-					return; // Detener la ejecución del código si falta algún campo o no se han aceptado los términos y condiciones
-				}
-
-				// Si todos los campos están completos y se han aceptado los términos y condiciones, continuar con la redirección
-				window.location.href = "pago.html";
-			});
-		}
-
-
-
-		/*BOTON BORRAR FORMULARIO*/
-
-		var borrarBtn = document.getElementById("borrar");
-		if (borrarBtn) {
-			borrarBtn.addEventListener("click", function() {
-				document.getElementById("inputNombre").value = "";
-				document.getElementById("inputApellidos").value = "";
-				document.getElementById("inputEmail4").value = "";
-				document.getElementById("inputAddress").value = "";
-				document.getElementById("inputAddress2").value = "";
-				document.getElementById("inputState").value = "";
-				document.getElementById("inputZip").value = "";
-				document.getElementById("inputCity").value = "";
-			});
-		}
-		//***********************PAGINA PAGO.HTML******************************//
 
 		/*Boton pago*/
 		var pagoBtn = document.getElementById("pago");
@@ -123,36 +144,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
 				// Expresión regular para verificar que solo se ingresen números
 				var regexNumeros = /^\d+$/;
+				// Variable para verificar si todas las condiciones se cumplen
+				var validacionExitosa = true;
 
-				// Verificar que el número de tarjeta contenga solo números  y tener 16 numeros
+				// Verificación de las celdas
 				if (!regexNumeros.test(numeroTarjeta) || numeroTarjeta.length !== 16) {
 					alert("El número de tarjeta debe contener solo números y tener 16 numeros.");
-					return; // Detener la ejecución del código si el número de tarjeta no es válido
-				}
-
-				// Verificar que el mes de vencimiento contenga solo números y esté en el rango válido (01-12)
-				if (!regexNumeros.test(mesVencimiento) || parseInt(mesVencimiento) < 1 || parseInt(mesVencimiento) > 12) {
+					validacionExitosa = false;
+				}else if (!regexNumeros.test(mesVencimiento) || parseInt(mesVencimiento) < 1 || parseInt(mesVencimiento) > 12) {
 					alert("El mes de vencimiento debe contener solo números y estar en el rango válido (01-12).");
-					return; // Detener la ejecución del código si el mes de vencimiento no es válido
-				}
-
-				// Verificar que el año de vencimiento contenga solo números y tenga 4 caracteres
-				if (!regexNumeros.test(anoVencimiento) || anoVencimiento.length !== 4) {
+					validacionExitosa = false;
+				}else if (!regexNumeros.test(anoVencimiento) || anoVencimiento.length !== 4) {
 					alert("El año de vencimiento debe contener solo números y tener 4 caracteres.");
-					return; // Detener la ejecución del código si el año de vencimiento no es válido
-				}
-
-				// Verificar que el CVV contenga solo números y tenga exactamente 3 caracteres
-				if (!regexNumeros.test(cvv) || cvv.length !== 3) {
+					validacionExitosa = false;
+				}else if (!regexNumeros.test(cvv) || cvv.length !== 3) {
 					alert("El CVV debe contener solo números y tener exactamente 3 caracteres.");
-					return; // Detener la ejecución del código si el CVV no es válido
-				}
-
-				// Verificar que se haya ingresado un nombre de tarjeta
-				if (nombreTarjeta.trim() === "") {
+					validacionExitosa = false;
+				}else if (nombreTarjeta.trim() === "") {
 					alert("Por favor, ingrese el nombre de la tarjeta.");
-					return; // Detener la ejecución del código si el campo del nombre de tarjeta está vacío
+					validacionExitosa = false;
 				}
+				
 
 				// Verificar si la tarjeta está caducada
 				var fechaActual = new Date();
@@ -161,15 +173,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
 				if (anoActual > parseInt(anoVencimiento) || (anoActual == parseInt(anoVencimiento) && mesActual > parseInt(mesVencimiento))) {
 					alert("La tarjeta está caducada. Por favor, introduzca una tarjeta válida.");
-					return; // Detener la ejecución del código si la tarjeta está caducada
+					validacionExitosa = false;
 				}
 
-				alert("¡Pago procesado correctamente!");
+				// Si todas las verificaciones son exitosas
+				if (validacionExitosa) {
+					
+
+					// Vaciar los campos de entrada después de mostrar el mensaje de éxito
+					document.getElementById("numero_tarjeta").value = "";
+					document.getElementById("mes").value = "";
+					document.getElementById("ano").value = "";
+					document.getElementById("ccv").value = "";
+					document.getElementById("nombre_tarjeta").value = "";
+
+					// Limpiar el carrito
+					localStorage.removeItem('carrito');
+					mostrarCarrito();
+					alert("¡Pago procesado correctamente, se ha enviado un correo!");
+					
+				}
 			});
 		}
+
+		console.log("Validación exitosa:", validacionExitosa);
+
 		//BOTON BORRAR CELDAS PAGO
-		// Verificar si el elemento "cancelar" existe antes de agregar el event listener
-		// Verificar si el elemento "cancelar" existe antes de agregar el event listener
 		var cancelarBtn = document.getElementById("cancelar");
 		if (cancelarBtn) {
 			cancelarBtn.addEventListener("click", function() {
@@ -180,4 +209,5 @@ document.addEventListener("DOMContentLoaded", function() {
 				document.getElementById("nombre_tarjeta").value = "";
 			});
 		}
-	}});
+	}
+});
